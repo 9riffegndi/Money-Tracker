b9f661aa1bcf2cd7d6a821907ff9ae7b9338c137
9riffegndi <marrief19@gmail.com>
2024-12-30 11:41:57 +0700

    del:: chart.js

--- src/Components/Analytics.jsx
+++ src/Components/Analytics.jsx
@@ -1,11 +1,8 @@
 import { Link } from "react-router-dom";
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { useState, useEffect } from "react";
 
 
 // Register the necessary Chart.js components
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default function Analytics() {
     const transaction = localStorage.getItem("transactions");
@@ -31,50 +28,12 @@ export default function Analytics() {
         );
     }
 
-    // Parse the transaction data from localStorage
-    const transactions = JSON.parse(transaction);
-
-    // Process the data for categories and their corresponding total incomes and expenses
-    const categories = [...new Set(transactions.map((t) => t.category))]; // Extract unique categories
-    const incomeData = categories.map((category) =>
-        transactions
-            .filter((transaction) => transaction.category === category)
-            .reduce((total, transaction) => total + transaction.income, 0)
-    );
-    const expenseData = categories.map((category) =>
-        transactions
-            .filter((transaction) => transaction.category === category)
-            .reduce((total, transaction) => total + transaction.expenses, 0)
-    );
-
-    // Chart data
-    const data = {
-        labels: categories,
-        datasets: [
-            {
-                label: 'Income',
-                data: incomeData,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 1,
-            },
-            {
-                label: 'Expenses',
-                data: expenseData,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgb(255, 99, 132)',
-                borderWidth: 1,
-            },
-        ],
-    };
-
     return (
         <div className="flex flex-col p-2 gap-2  w-full h-screen">
             <p className="badge badge-primary">Analytics</p>
 
             <div className=" grid grid-cols-12 h-screen  w-full">
                 <div className="w-full col-span-12">
-                    <Bar className="w-full" data={data} />
                 </div>
             </div>
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
+import "./chartist.css"; // Import Chartist CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
